9465c811285ec0bbedebeccf07f4183927aee67b
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-01-31 01:57:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,6 +4,7 @@ def square_array(array)
   new_array = []
   while array[counter] <= array.length do
     square = array[counter] ** 2
+    new_array.push(square)
     counter += 1
     print new_array
   end
